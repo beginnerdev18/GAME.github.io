0e8344f400ec4a01948d7180ca528c5a3655e998
regex <neogabar@gmail.com>
2022-02-05 18:47:04 -0600

    game_enemy_1.0

--- canvas/canvas-Game/index.js
+++ canvas/canvas-Game/index.js
@@ -49,20 +49,56 @@ class Proyectile{
 
 }
 
+class Enemy{
+    constructor(x,y,radius,color,velocity) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.velocity=velocity
+    }
+
+    draw() {
+        cxt.beginPath()
+        cxt.arc (
+            this.x,this.y,this.radius,
+            0,Math.PI * 2, false);
+        cxt.fillStyle =this.color 
+        cxt.fill()
+        
+    }
+
+    update() {
+        this.draw()
+        this.x = this.x + this.velocity.x
+        this.y = this.y + this.velocity.y 
+
+    }
+
+}
+
 const x = canvas.width / 2
 const y = canvas.height / 2
 
 const player = new Player (x, y,50 ,"pink")
-
-
-
-
-
 const proyectiles = [ ]
+const enemies =[]
 
+function spawnEnemies(){
+    setInterval(( ) => {
+        const x = 100
+        const y = 100
+        const radius = 30
+        const color = "green"
+        const velocity ={
+            x:1,y:1
+        }
+        enemies.push(new Enemy(x,y,radius, color,velocity))
 
-
-
+        console.log(enemies)
+    },1000)
+        
+}
 
 
 
@@ -72,9 +108,13 @@ function animate() {
     player.draw()
     proyectiles.forEach((proyectile) => {
     proyectile.update()
-      
     })
-}
+
+    enemies.forEach((enemy ) => {
+     enemy.update()
+    })
+
+    }
 
  addEventListener("click",(event) => {
      const angle = Math.atan2(
@@ -96,3 +136,4 @@ function animate() {
  })
 
    animate()
+   spawnEnemies()
